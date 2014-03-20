0d8bdef5282e1c05b4fc454a2beda75479a68733
Tianon Gravi <admwiggin@gmail.com>
2014-03-20 00:30:34 -0600

    Improve build script for all this nonsense

--- src/make.sh
+++ src/make.sh
@@ -7,6 +7,18 @@ tmp="$(readlink -f ../tmp)"
 out="$(readlink -f ../out)"
 
 (
+	set -x
 	cd oneinit
 	go build -o "$tmp/oneinit" -ldflags "-d -X main.version $ver"
 )
+
+(
+	set -x
+	cd ../../linux
+	make
+)
+
+cp -v ../../linux/arch/x86/boot/bzImage "$tmp/linux"
+
+# TODO create initrd.gz
+# TODO create hybrid ISO
